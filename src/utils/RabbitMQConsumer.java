5f7bbaf9345349f33a9b2f8cca4152d63fdab17c
Brielino <gabrieletummolo@libero.it>
2021-07-01 19:37:23 +0200

    aggiunti file output csv

--- src/utils/RabbitMQConsumer.java
+++ src/utils/RabbitMQConsumer.java
@@ -17,8 +17,14 @@ public class RabbitMQConsumer {
 
         RabbitMQManager rmq = new RabbitMQManager(rabbitMQ, rabbitMQUsername, rabbitMQPassword, rabbitMQQueue);
 
-        System.out.println(rmq.createDetachedReader(rabbitMQQueue));	//RabbitMQ reader
-
+        rmq.createDetachedReader(rabbitMQQueue);	//RabbitMQ reader
+        if(rabbitMQQueue.equals("query1")){
+            System.out.println("ts,id_cella,ship_t35,avg_t35,ship_t60,avg_t60,ship_t70,avg_t70,ship_to,avg_to");
+        }else if(rabbitMQQueue.equals("query2")){
+            System.out.println("ts,sea,slot_a,rank_a,slot_b,rank_b");
+        }else if(rabbitMQQueue.equals("query3")){
+            System.out.println("ts,trip_1,rating_1,trip_2,rating_2,trip_3,rating_3,trip_4,rating_4,trip_5,rating_5");
+        }
         while(true){
             Thread.sleep(500);
         }
